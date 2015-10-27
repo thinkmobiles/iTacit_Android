96b842f4b272a50eb36136b515929ad4773b4fa1
vasiliy <vasiachess@gmail.com>
2015-10-27 13:15:24 +0200

    Add Wheel DatePicker

---
+++ app/src/main/java/com/itacit/healthcare/presentation/news/presenters/NewsFilterPresenterImp.java
@@ -0,0 +1,8 @@
+package com.itacit.healthcare.presentation.news.presenters;
+
+/**
+ * Created by root on 26.10.15.
+ */
+public class NewsFilterPresenterImp extends NewsSearchFilterPresenter {
+
+}
