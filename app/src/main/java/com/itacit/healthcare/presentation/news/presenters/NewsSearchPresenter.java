01be3b774a49a24350c64e02d1bb32fb354dccd9
vasiliy <vasiachess@gmail.com>
2015-11-04 13:32:22 +0200

    Add filters to FilterEditText in NewsSearchFragment

--- app/src/main/java/com/itacit/healthcare/presentation/news/presenters/NewsSearchPresenter.java
+++ app/src/main/java/com/itacit/healthcare/presentation/news/presenters/NewsSearchPresenter.java
@@ -1,10 +1,19 @@
 package com.itacit.healthcare.presentation.news.presenters;
 
+import com.itacit.healthcare.data.entries.Author;
+import com.itacit.healthcare.data.entries.Category;
 import com.itacit.healthcare.domain.interactor.GetAuthorsUseCase;
 import com.itacit.healthcare.domain.interactor.GetCategoriesUseCase;
 import com.itacit.healthcare.presentation.base.presenters.BasePresenter;
+import com.itacit.healthcare.presentation.base.widgets.chipsView.Filter;
+import com.itacit.healthcare.presentation.news.fragments.NewsSearchFragment;
+import com.itacit.healthcare.presentation.news.mapper.AuthorModelMapper;
+import com.itacit.healthcare.presentation.news.mapper.CategoryModelMapper;
+import com.itacit.healthcare.presentation.news.models.AuthorModel;
+import com.itacit.healthcare.presentation.news.models.CategoryModel;
 import com.itacit.healthcare.presentation.news.views.INewsSearchView;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -17,10 +26,16 @@ import rx.schedulers.TimeInterval;
 public class NewsSearchPresenter extends BasePresenter<INewsSearchView> implements INewsSearchPresenter {
     private GetAuthorsUseCase getAuthorsUseCase;
     private GetCategoriesUseCase getCategoriesUseCase;
+    private AuthorModelMapper authorMapper;
+    private CategoryModelMapper categoryMapper;
+	private List<AuthorModel> authorModels;
+	private List<CategoryModel> categoryModels;
 
-    public NewsSearchPresenter(GetAuthorsUseCase getAuthorsUseCase, GetCategoriesUseCase getCategoriesUseCase) {
+    public NewsSearchPresenter(GetAuthorsUseCase getAuthorsUseCase, GetCategoriesUseCase getCategoriesUseCase, AuthorModelMapper authorModelMapper, CategoryModelMapper categoryModelMapper) {
         this.getAuthorsUseCase = getAuthorsUseCase;
         this.getCategoriesUseCase = getCategoriesUseCase;
+        authorMapper = authorModelMapper;
+        categoryMapper = categoryModelMapper;
     }
 
     @Override
@@ -48,8 +63,45 @@ public class NewsSearchPresenter extends BasePresenter<INewsSearchView> implemen
         getCategoriesUseCase.execute(new GetCategoriesSubscriber());
     }
 
+    @Override
+    public void selectAuthorFilterById(long id) {
 
-    private final class GetAuthorsSubscriber extends Subscriber {
+	    Filter filter;
+	    for (AuthorModel authorModel : authorModels) {
+		    if (authorModel.getId() == id) {
+			    filter = new Filter(id, authorModel.getFullName(), Filter.FilterType.Author);
+			    if(getView() != null) getView().addItemToSearchList(filter);
+		    }
+	    }
+
+    }
+
+	@Override
+	public void selectCategoryFilterById(long id) {
+
+		Filter filter;
+		for (CategoryModel categoryModel : categoryModels) {
+			if (categoryModel.getId() == id) {
+				filter = new Filter(id, categoryModel.getName(), Filter.FilterType.Category);
+				if(getView() != null) getView().addItemToSearchList(filter);
+			}
+		}
+
+	}
+
+    private void showAuthorsOnView(List<Author> authors) {
+        List<AuthorModel> authorModels = authorMapper.transform(authors);
+	    this.authorModels = authorModels;
+        if(getView() != null) getView().showAuthors(authorModels);
+    }
+
+    private void showCategoriesOnView(List<Category> categories) {
+        List<CategoryModel> categoryModels = categoryMapper.transform(categories);
+	    this.categoryModels = categoryModels;
+        if(getView() != null) getView().showCategories(categoryModels);
+    }
+
+    private final class GetAuthorsSubscriber extends Subscriber<List<Author>> {
 
         @Override
         public void onCompleted() {
@@ -62,11 +114,11 @@ public class NewsSearchPresenter extends BasePresenter<INewsSearchView> implemen
         }
 
         @Override
-        public void onNext(Object o) {
-
+        public void onNext(List<Author> authors) {
+            showAuthorsOnView(authors);
         }
     }
-    private final class GetCategoriesSubscriber extends Subscriber {
+    private final class GetCategoriesSubscriber extends Subscriber<List<Category>> {
 
         @Override
         public void onCompleted() {
@@ -79,9 +131,10 @@ public class NewsSearchPresenter extends BasePresenter<INewsSearchView> implemen
         }
 
         @Override
-        public void onNext(Object o) {
-
+        public void onNext(List<Category> categories) {
+            showCategoriesOnView(categories);
         }
+
     }
 
 }
