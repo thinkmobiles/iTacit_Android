73f6f18184d4143b3c6c0773a3caf52ee846e2da
root <daniil.pozhidaev@gmail.com>
2015-11-13 13:07:29 +0200

    change rxBus app monitoring auth error

---
+++ app/src/main/java/com/itacit/healthcare/presentation/auth/views/LoginView.java
@@ -0,0 +1,11 @@
+package com.itacit.healthcare.presentation.auth.views;
+
+import com.itacit.healthcare.presentation.base.views.View;
+
+/**
+ * Created by root on 10.11.15.
+ */
+public interface LoginView extends View {
+    String getUserName();
+    String getPassword();
+}
